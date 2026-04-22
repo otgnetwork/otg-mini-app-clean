521492b56c6acd086c3a66dc03ab2acf9380592f
otgnetwork <tiktokwallker@gmail.com>
2026-04-22 09:25:55 +0300

    Fix root route

--- server.py
+++ server.py
@@ -5,7 +5,10 @@ from socketserver import TCPServer
 PORT = int(os.environ.get("PORT", 8000))
 
 class Handler(SimpleHTTPRequestHandler):
-    pass
+    def do_GET(self):
+        if self.path == "/":
+            self.path = "/index.html"
+        return super().do_GET()
 
 if __name__ == "__main__":
     with TCPServer(("", PORT), Handler) as httpd:
